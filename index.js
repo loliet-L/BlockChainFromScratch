e1deea178108d76b16b1ed4b5c543a265639b39e
Vivek Lone <79907341+loliet-L@users.noreply.github.com>
2023-01-16 20:45:21 +0530

    added syncing blockchain

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express= require('express');
 const BlockChain = require('./blockchain');
 const bodyParser = require("body-parser");
 const PubSub = require("./publishsubscribe");
+const request = require("request");
 
 const app=express();
 const blockchain = new BlockChain();
 const pubsub = new  PubSub({blockchain});
+const DEFAULT_PORT=3000;
+
+const ROOT_NODE_ADDRESS=`http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(()=>pubsub.broadcastChain(),1000);
 
@@ -23,7 +27,20 @@ app.post('/api/mine',(req,res)=>{
     res.redirect('/api/blocks');
 })
 
-const DEFAULT_PORT=3000;
+const syncChain= ()=>{
+   request({ url:`${ROOT_NODE_ADDRESS}/api/blocks`},(error,response,body)=>{
+     if(!error && response.statusCode===200)
+     {
+        const rootChain = JSON.parse(body);
+        console.log("replace chain on Sync with ",rootChain);
+        blockchain.replaceChain(rootChain);
+     }
+
+   });
+
+}
+
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT==='true')
@@ -35,4 +52,5 @@ const PORT= PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT,()=>{
     console.log(`listening to port: ${PORT}`);
+    syncChain();
 });
